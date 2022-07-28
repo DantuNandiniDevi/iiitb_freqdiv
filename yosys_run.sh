5768fcb64a022c4d8ac06a081879ea68136c0426
DantuNandiniDevi <62461290+DantuNandiniDevi@users.noreply.github.com>
2022-07-28 12:04:05 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,14 +1,14 @@
 # read design
 
-read_verilog 32bit_carry_ripple_adder.v
+read_verilog /home/nandini.dantu/Desktop/iiitb_freqdiv/verilog/iiitb_freqdiv.v
 
 # generic synthesis
-synth -top ripple_carry_adder
+synth -top iiitb_freqdiv
 
 # mapping to mycells.lib
-dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
-abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+dfflibmap -liberty ‌‌‌‌‌‌/home/nandini.dantu/Desktop/iiitb_freqdiv/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/nandini.dantu/Desktop/iiitb_freqdiv/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog -assert synth_ripple_carry_adder.v
+write_verilog -noattr iiitb_freqdiv_synth.v
