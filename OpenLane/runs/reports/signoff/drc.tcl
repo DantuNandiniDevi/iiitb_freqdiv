cb4af714527884077c573809fad73566fd38bf0e
DantuNandiniDevi <62461290+DantuNandiniDevi@users.noreply.github.com>
2022-08-31 02:50:50 +0530

    Add files via upload

---
+++ OpenLane/runs/reports/signoff/drc.tcl
@@ -0,0 +1,25 @@
+box 42.590um 46.430um 42.860um 47.655um; feedback add "N-well spacing < 1.27um (nwell.2a)" medium
+box 46.270um 40.990um 46.540um 42.215um; feedback add "N-well spacing < 1.27um (nwell.2a)" medium
+box 55.930um 40.990um 56.200um 42.215um; feedback add "N-well spacing < 1.27um (nwell.2a)" medium
+box 46.730um 30.110um 47.000um 31.335um; feedback add "N-well spacing < 1.27um (nwell.2a)" medium
+box 51.330um 30.110um 51.600um 31.335um; feedback add "N-well spacing < 1.27um (nwell.2a)" medium
+box 41.545um 47.345um 41.715um 47.515um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 41.545um 47.345um 41.715um 47.515um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 42.465um 47.345um 42.635um 47.515um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 42.465um 47.345um 42.635um 47.515um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 45.225um 41.905um 45.395um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 45.225um 41.905um 45.395um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 46.145um 41.905um 46.315um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 46.145um 41.905um 46.315um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 54.885um 41.905um 55.055um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 54.885um 41.905um 55.055um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 55.805um 41.905um 55.975um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 55.805um 41.905um 55.975um 42.075um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 45.685um 31.025um 45.855um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 45.685um 31.025um 45.855um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 46.605um 31.025um 46.775um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 46.605um 31.025um 46.775um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 50.285um 31.025um 50.455um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 50.285um 31.025um 50.455um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 51.205um 31.025um 51.375um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
+box 51.205um 31.025um 51.375um 31.195um; feedback add "Local interconnect minimum area < 0.0561um^2 (li.6)" medium
