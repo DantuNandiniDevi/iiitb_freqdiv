14d0beff53ffd54bc6e4602d8018f178c340805c
DantuNandiniDevi <62461290+DantuNandiniDevi@users.noreply.github.com>
2022-08-31 03:08:51 +0530

    Add files via upload

---
+++ OpenLane/runs/config_in.tcl
@@ -0,0 +1,16 @@
+set ::env(PDK) {sky130A}
+set ::env(STD_CELL_LIBRARY) {sky130_fd_sc_hd}
+set ::env(DESIGN_NAME) {iiitb_freqdiv}
+set ::env(VERILOG_FILES) {/openlane/designs/iiitb_freqdiv/src/iiitb_freqdiv.v}
+set ::env(CLOCK_PORT) {clkin}
+set ::env(CLOCK_NET) {clkin}
+set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) {1}
+set ::env(CLOCK_PERIOD) {10}
+set ::env(PL_TARGET_DENSITY) {0.7}
+set ::env(FP_SIZING) {relative}
+set ::env(FP_CORE_UTIL) {20}
+set ::env(LIB_SYNTH) {/openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__typical.lib}
+set ::env(LIB_FASTEST) {/openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__fast.lib}
+set ::env(LIB_SLOWEST) {/openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__slow.lib}
+set ::env(LIB_TYPICAL) {/openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__typical.lib}
+set ::env(TEST_EXTERNAL_GLOB) {/openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__slow.lib /openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__fast.lib /openlane/designs/iiitb_freqdiv/src/sky130_vsdinv.lef /openlane/designs/iiitb_freqdiv/src/iiitb_freqdiv.v /openlane/designs/iiitb_freqdiv/src/sky130_fd_sc_hd__typical.lib}
